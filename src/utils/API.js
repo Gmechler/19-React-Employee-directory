82fdeb739991f692da3e51d2cfe650a6b00e2651
Grayson Mechler <Graysonmechler@gmail.com>
2020-03-07 13:26:56 -0600

    unstyled working

---
+++ src/utils/API.js
@@ -0,0 +1,9 @@
+// import axios from "axios";
+
+// // exports a JSON object returned from the random users API
+
+// export default {
+//   createUser: function() {
+//     return axios.get("https://randomuser.me/api/?results=150");
+//   }
+// };
